1c5acc26df321d1afe803ef6f29d60d6ecbf452b
Nick Vandenberge <nvandenberge14@gmail.com>
2021-01-02 15:27:25 -0500

    added localStorage feature for save button

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
+// checking localStorage for events and adding them to timeSlots
+$(document).ready(function() {
+  if(localStorage.getItem("events") !== null) {
+    dailyEventsArr = JSON.parse(localStorage.getItem("events"));
+    dailyEventsArr.forEach(function(event) {
+      $("#" + event.id).html(event.event) 
+    })
+  }
+})
+
 // using Moment.js to display the current date at top
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
@@ -6,6 +16,7 @@ const $startTime = moment().utc().set({ hour: 9, minute: 00 });
 const $endTime = moment().utc().set({ hour: 17, minute: 00 });
 const $timeSlots = [];
 
+// while loop to generate timeSlots between 9am-5pm
 while ($startTime <= $endTime) {
   $timeSlots.push(new moment($startTime));
   $startTime.add(1, "hour");
@@ -26,10 +37,21 @@ $.each($timeSlots, function(index, hour) {
   const $saveBtn = $("<div>").addClass("col-1 saveBtn");
   const $saveIcon = $("<i>").addClass("far fa-save saveIcon");
 
+  // adding planner elements to container
   $(".container").append($hourlyRow);
   $timeColumn.text(hour.format("h A"));
   $hourlyRow.append($timeColumn);
   $hourlyRow.append($eventInput);
   $hourlyRow.append($saveBtn);
   $saveBtn.append($saveIcon);
+});
+
+// event listener for saveBtn to add input to localStorage
+$(".saveBtn").click(function() {
+  let dailyEventsArr = JSON.parse(localStorage.getItem("events")) || [];
+  dailyEventsArr.push({
+    event: $(this).prev().val(),
+    id: $(this).prev().attr('id'),
+  });
+  localStorage.setItem("events", JSON.stringify(dailyEventsArr))
 });
